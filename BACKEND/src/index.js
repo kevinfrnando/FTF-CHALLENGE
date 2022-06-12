f073b27efc20ae9a94c533c2f53c190ad1328d7e
Kevin <kevin_vergara.rodriguez@yahoo.es>
2022-06-11 21:26:13 -0500

    Backend Api

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -1,15 +1,34 @@
-const http = require("http");
-require('dotenv').config();
+import http from 'http';
+import 'dotenv/config';
+import axios from 'axios';
 
 const server = http.createServer(( req, res )=>{
     const { url, method } = req;
-    if( method === "GET" && url === "/"){
-        res.writeHead(200, {'Content-Type':'application/json'} );
-        res.write(JSON.stringify({message: 'HELLO WORLDS'}));
+
+    if( method === "GET" && ( url === "/user" || url === "/repo")){
+        const endPoint = url === "/user" ? "/users/kevinfrnando": "/repos/kevinfrnando/FTF-CHALLENGE/commits";
+        try{
+            axios.get( `${process.env.API}${endPoint}`, {
+                headers: {
+                    'Authorization': `token ${process.env.API_KEY}`
+                }  
+            } ).then( r => {
+                res.writeHead(200, {'Content-Type':'application/json'} );
+                res.write(JSON.stringify( r.data ));
+                res.end();
+            })
+        }catch( e ){
+            res.writeHead(200, {'Content-Type':'application/json'} );
+            res.write(JSON.stringify([{}]));
+            res.end();
+        }
+    }else{
+        res.writeHead(404, {'Content-Type':'application/json'} );
+        res.write(JSON.stringify([{}]));
         res.end();
     }
+
 });
 
 server.listen( process.env.PORT );
 
-console.log('Server on port', process.env.PORT);
